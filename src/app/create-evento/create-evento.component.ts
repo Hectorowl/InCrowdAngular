f766da19d47545c7e2abf1c6f85721fe1b57aab2
hector <hector.ninian@gmail.com>
2023-06-25 00:17:22 +0200

    validador creacion evento

--- src/app/create-evento/create-evento.component.ts
+++ src/app/create-evento/create-evento.component.ts
@@ -13,11 +13,11 @@ import {NgbTimeStructAdapter} from "@ng-bootstrap/ng-bootstrap/timepicker/ngb-ti
 })
 export class CreateEventoComponent implements OnInit {
 
-  user : string;
+  user: string;
 
-  nombre : string;
-  descripcion : string;
-  fecha : NgbDateStruct;
+  nombre: string;
+  descripcion: string;
+  fecha: NgbDateStruct;
   hora: NgbTimeStruct;
   esPublico: boolean;
   aforo: number;
@@ -28,19 +28,24 @@ export class CreateEventoComponent implements OnInit {
   showSuccess = false;
   showNameError = false;
   showOtherError = false;
+  showReqErrorName: boolean = false;
+  showReqErrorCat: boolean = false;
+  showReqErrorDes: boolean = false;
+  showReqErrorAforo: boolean = false;
+  showReqErrorFecha: boolean = false;
+  showReqErrorHora: boolean = false;
 
 
-
-  constructor( private http: HttpClient, private router: Router, private us:UserdataService){
-  this.us.getLan().subscribe(us => this.user = us);
+  constructor(private http: HttpClient, private router: Router, private us: UserdataService) {
+    this.us.getLan().subscribe(us => this.user = us);
   }
 
   ngOnInit(): void {
-    this.user='hectoruser';
+    this.user = 'hectoruser';
 
     this.nombre = '';
     this.descripcion = '';
-    this.fecha= {
+    this.fecha = {
       day: 0,
       month: 0,
       year: 0
@@ -59,17 +64,24 @@ export class CreateEventoComponent implements OnInit {
 
   }
 
-  clean(){
+  clean() {
     this.showSuccess = false;
     this.showNameError = false;
     this.showOtherError = false;
+    this.showReqErrorName = false;
+    this.showReqErrorCat = false;
+    this.showReqErrorDes = false;
+    this.showReqErrorAforo = false;
+    this.showReqErrorFecha = false;
+    this.showReqErrorHora = false;
   }
 
-  cleanForm(){
+  cleanForm() {
+    this.clean()
 
     this.nombre = '';
     this.descripcion = '';
-    this.fecha= {
+    this.fecha = {
       day: 0,
       month: 0,
       year: 0
@@ -86,40 +98,70 @@ export class CreateEventoComponent implements OnInit {
 
   createEvento() {
     this.clean();
+    if (this.valid()) {
 
-    console.log(this.fecha)
-    const eventoData = {
-      nombre: this.nombre,
-      descripcion: this.descripcion,
-      fecha: this.fecha.day.toString()+'-'+this.fecha.month+'-'+this.fecha.year,
-      hora: this.hora.hour+':'+this.hora.minute,
-      esPublico: this.esPublico,
-      aforo: this.aforo,
-      categoria: this.categoria,
-      organizador: this.organizador,
-    };
-    console.log(eventoData)
+      console.log(this.fecha)
+      const eventoData = {
+        nombre: this.nombre,
+        descripcion: this.descripcion,
+        fecha: this.fecha.day.toString() + '-' + this.fecha.month + '-' + this.fecha.year,
+        hora: this.hora.hour + ':' + this.hora.minute,
+        esPublico: this.esPublico,
+        aforo: this.aforo,
+        categoria: this.categoria,
+        organizador: this.organizador,
+      };
+      console.log(eventoData)
 
-    this.http.post('http://localhost:4200/api' + '/CreateEvento/', eventoData).subscribe(
-      (resp: any) => {
-        if(resp.success==true){
-          this.showSuccess = true;
-          setTimeout(() => {
-            this.clean();
-          }, 2000);
-        }else{
-          if(resp.message=="Ya existe un evento con ese nombre"){
-            this.showNameError = true;
-          }
-          else{
+      this.http.post('http://localhost:4200/api' + '/CreateEvento/', eventoData).subscribe(
+        (resp: any) => {
+          if (resp.success == true) {
+            this.showSuccess = true;
+            setTimeout(() => {
+              this.cleanForm();
+            }, 2000);
+          } else {
+            if (resp.message == "Ya existe un evento con ese nombre") {
+              this.showNameError = true;
+            } else {
               this.showOtherError = true;
             }
-        }
-      },
-      (error: HttpErrorResponse) => {
-        this.showOtherError=true
-        console.error(error);
-      });
+          }
+        },
+        (error: HttpErrorResponse) => {
+          this.showOtherError = true
+          console.error(error);
+        });
+    }
+  }
+
+  valid() {
+    var ret = true;
+    if (this.nombre == '') {
+      this.showReqErrorName = true;
+      ret = false;
+    }
+    if (this.categoria == '') {
+      this.showReqErrorCat = true;
+      ret = false;
+    }
+    if (this.descripcion == '') {
+      this.showReqErrorDes = true;
+      ret = false;
+    }
+    if (this.aforo == 0) {
+      this.showReqErrorAforo = true;
+      ret = false;
+    }
+    if (this.fecha.day == 0 && this.fecha.month == 0 && this.fecha.year == 0 ) {
+      this.showReqErrorFecha = true;
+      ret = false;
+    }
+    if (this.hora.hour == 0 && this.hora.minute == 0) {
+      this.showReqErrorHora = true;
+      ret = false;
+    }
+    return ret
   }
 }
 
